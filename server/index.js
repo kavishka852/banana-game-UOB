43414e55b680ad682b0fd32df335e2515103f1ac
Kavishka Roshan <weerawardhanakr@gmail.com>
2024-10-08 09:33:28 +0530

    first commit

---
+++ server/index.js
@@ -0,0 +1,21 @@
+const express = require("express")
+const mongoose = require('mongoose')
+const cors = require("cors")
+const UserModel = require('./models/User')
+
+
+const app = express()
+app.use(express.json())
+app.use(cors())
+
+mongoose.connect("mongodb://127.0.0.1:27017/bananagame")
+
+app.post('/register', (req, res) => {
+    UserModel.create(req.body)
+    .then(users => res.json(users))
+    .catch(err => res.json(err))
+})
+
+app.listen(3001, () =>{
+    console.log("Server running!")
+})
